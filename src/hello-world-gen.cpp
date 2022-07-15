3c172bfcc508e43439d22a5199748bc0983c6992
Arthur Hlaing <arhlaing@microsoft.com>
2022-07-15 15:41:43 -0700

    Add project files.

---
+++ src/hello-world-gen.cpp
@@ -0,0 +1,12 @@
+﻿// hello-world-gen.cpp : Defines the entry point for the application.
+//
+
+#include "hello-world-gen.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
